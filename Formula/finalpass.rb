89a80f816ca91d0f6bf048a4e2ba0e12f2f5e838
Gabe Morales <gabomorales@me.com>
2026-05-29 13:34:29 -0500

    Finalpass 0.1.6

--- Formula/finalpass.rb
+++ Formula/finalpass.rb
@@ -1,8 +1,8 @@
 class Finalpass < Formula
   desc "Execution-first pre-PR checks for flaky tests and mutation coverage"
   homepage "https://finalpass.dev"
-  url "https://registry.npmjs.org/@finalpass/finalpass-cli/-/finalpass-cli-0.1.5.tgz"
-  sha256 "0a5d22621a65839db5da6ac18c3d3021e530873b8a755b588b3516b0c5bc36f7"
+  url "https://registry.npmjs.org/@finalpass/finalpass-cli/-/finalpass-cli-0.1.6.tgz"
+  sha256 "179c9329c49aeab56a44f4fc10ebdd423ea729b6b28bd6999934a914bec9fd17"
   license "MIT"
   depends_on "node"
   def install
@@ -11,6 +11,6 @@ class Finalpass < Formula
   end
   test do
     output = shell_output("#{bin}/finalpass --version")
-    assert_match "0.1.5", output
+    assert_match "0.1.6", output
   end
 end
